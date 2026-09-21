52455b3224dfa66d8107090e8e747d639f12fa4a
fishsauce-05 <fishsauce-05@users.noreply.github.com>
2026-09-21 13:49:33 +0700

    more details for abstract classes

--- views/base/ui_input.py
+++ views/base/ui_input.py
@@ -1,15 +1,17 @@
 from abc import ABC, abstractmethod
 from typing import Callable
 
+from views.base.field import Field
+
 class UIInput(ABC):
     @abstractmethod
-    def dropdown_input(self, options: list[str], on_select: Callable[[str], None]):
+    def dropdown_input(self, options: list[str], on_select: Callable[[str], None]) -> Field:
         pass
     
     @abstractmethod
-    def str_input(self, row: int, req: dict):
+    def str_input(self, row: int, req: dict[str, str]) -> Field:
         pass
     
     @abstractmethod
-    def radio_input(self, label: str, options: list[str]):
+    def radio_input(self, label: str, options: list[str]) -> Field:
         pass
--- views/base/view.py
+++ views/base/view.py
@@ -3,25 +3,25 @@ from typing import Callable
 
 class View(ABC):    
     @abstractmethod
-    def select_project_type(self, project_type: str, on_select: Callable[[str], None]):
+    def select_project_type(self, project_type: str, on_select: Callable[[str], None]) -> None:
         pass
     
     @abstractmethod
-    def select_project_input(self, requirements: dict, on_submit: Callable[[dict], None]):
+    def select_project_input(self, requirements: dict[str, dict[str, str]], on_submit: Callable[[dict], None]) -> None:
         pass
     
     @abstractmethod
-    def run(self):
+    def run(self) -> None:
         pass
     
     @abstractmethod
-    def close(self):
+    def close(self) -> None:
         pass
     
     @abstractmethod
-    def show_error(self, message: str):
+    def show_error(self, message: str) -> None:
         pass    
     
     @abstractmethod
-    def render_form(self, requirements: dict, on_submit: Callable[[dict], None]):
+    def render_form(self, requirements: dict[str, dict[str, str]], on_submit: Callable[[dict], None]) -> None:
         pass
